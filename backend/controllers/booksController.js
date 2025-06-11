193214823d5fdceddbc84d744a0d95a4ff516594
Student <student@Admins-MBP.lan>
2025-06-11 15:20:53 -0400

    A lot of cool features

--- backend/controllers/booksController.js
+++ backend/controllers/booksController.js
@@ -4,17 +4,31 @@ export const getFinished = async (req, res) => {
     try {
       const dbResponse = await db.query("SELECT * FROM books WHERE finished = true");
       const result = dbResponse.rows.map ((row) => (
-        {Genre: row.genre, Title: row.title, Author: row.author, Rating: parseFloat(row.rating.toFixed(1))
-        }
-      ))
-      res.json(result)
+        {Genre: row.genre, Title: row.title, Author: row.author, Rating: parseFloat(row.rating.toFixed(1)),
+        
+        }));
+      res.json(result);
     } catch (error) {
-      console.error("Could not show data:", error)
-      res.status(500).json({ error:"Server error" })
+      console.error("Could not show data:", error);
+      res.status(500).json({ error:"Server error" });
     }
-  }
+  };
 
 
-
+export const getUnfinished = async (req, res ) => {
+    try {
+        const dbResponse = await db.query("SELECT * FROM books WHERE finished = false");
+        const result = dbResponse.rows.map((row) => ({
+            Genre: row.genre,
+            Title: row.title,
+            Author: row.author,
+            Rating: parseFloat(row.rating.toFixed(1)),
+        }));
+        res.json(result);
+    } catch (error) {
+        console.error("couldn't show data:", error);
+        res.status(500).json({ error: "Server error"})
+    }
+};
 
 
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 import express from "express";
+import cors from "cors";
 import * as db from "./db/index.js";
 import "dotenv/config";
-import booksRoutes from "./routes/books.js"
+import booksRoutes from "./routes/books.js";
+
 
 const requestHandler = express();
 
 const port = process.env.PORT;
+
+
+//important right here to allow cross-origin requests(without cors, browsers block that connection)
+requestHandler.use(cors());
+
+
+
 requestHandler.use(express.json());
 requestHandler.listen(port, () => {
     console.log(`Server listening on port ${port}`);
@@ -17,7 +26,7 @@ requestHandler.use("/api/v1", booksRoutes);
 requestHandler.post("/api/v1/add", async (req, res) => {
     console.log(req.body);
     try {
-        const results = await db.query("INSERT INTO books (genre, title, author, rating) values ($1, $2, $3, $4) returning *", [req.body.Genre, req.body.Title, req.body.Author, req.body.Rating]);
+        const results = await db.query("INSERT INTO books (genre, title, author, rating, finished) values ($1, $2, $3, $4) returning *", [req.body.Genre, req.body.Title, req.body.Author, req.body.Rating, req.body.Finished]);
         console.log(results);
         res.status(201).json({
             status: "success",
