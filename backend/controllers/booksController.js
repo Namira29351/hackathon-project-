d94ccf3e9558f0c63c72c51a9891bb1a542c56e9
Student <student@Admins-MacBook-Pro.local>
2025-06-15 01:01:14 -0400

    I added all the comments for the backend

--- backend/controllers/booksController.js
+++ backend/controllers/booksController.js
@@ -1,5 +1,9 @@
 import * as db from "../db/index.js";
 
+// These route handlers use the imported db to run queries in our database. The db is responsible for running the actual queries. The route handlers use Express send a JSON response back to teh frontend.
+
+// The database gets all the books that have their "finished" column set to true. An SQL query is ran to select all the finished books and each each row is mapped to a new object. The rating field is formatted to be rounded up to one decimal point. The response is sent back as JSON object.
+
 export const getFinished = async (req, res) => {
     try {
       const dbResponse = await db.query("SELECT * FROM books WHERE finished = true");
@@ -14,6 +18,7 @@ export const getFinished = async (req, res) => {
     }
   };
 
+// The database gets all the books that have their "finished" column set to false. An SQL query is ran to select all the unfinished books and each each row is mapped to a new object. The rating field is formatted to be rounded up to one decimal point. The response is sent back as JSON object.
 
 export const getUnfinished = async (req, res ) => {
     try {
@@ -31,7 +36,7 @@ export const getUnfinished = async (req, res ) => {
     }
 };
 
-
+// This function runs an SQL query to delete the book that has the same title as the one from the request body. (when the user clicks the delete button, the title of the book is in the request body that is sent to the backend)
 
 export const deleteBook = async (req, res) => {
   try {
